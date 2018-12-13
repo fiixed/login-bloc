34da9b74b55548ae446a0e905fd8f1362b629dae
Andrew Bell <fiixed@gmail.com>
2018-12-13 14:14:37 -0600

    finished bloc

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -1,9 +1,22 @@
 import 'dart:async';
+import 'validators.dart';
 
-class Bloc {
-  final _emailController = StreamController<String>();
-  final _passwordController = StreamController<String>();
+class Bloc with Validators {
+  final _email = StreamController<String>();
+  final _password = StreamController<String>();
 
-  Function(String) get changeEmail => _emailController.sink.add;
-  
+  // Add data to stream
+  Stream<String> get email => _email.stream.transform(validateEmail);
+  Stream<String> get password => _password.stream.transform(validatePassword);
+
+  // Change data
+  Function(String) get changeEmail => _email.sink.add;
+  Function(String) get changePassword => _password.sink.add;
+
+  dispose() {
+    _email.close();
+    _password.close();
+  }
 }
+
+final bloc = Bloc();
